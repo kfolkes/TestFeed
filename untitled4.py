a18d3cb04363fde16de59fc39fd38594f89f11b8
kfolkes <krystl.folkes@gmail.com>
2016-11-05 05:56:31 -0400

    updated the home and the index pages for main application.

--- untitled4.py
+++ untitled4.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/home')
+def render_home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
